371c629ab2f5f0d81f73236c316824ab689a1cb3
Nagi <nagi@poifuture.com>
2019-07-01 06:26:56 +0800

    Cleanup Template

--- MarkdownStyle/src/taskpane/index.tsx
+++ MarkdownStyle/src/taskpane/index.tsx
@@ -11,7 +11,7 @@ initializeIcons()
 
 let isOfficeInitialized = false
 
-const title = "Contoso Task Pane Add-in"
+const title = "Markdown Style"
 
 const render = Component => {
   ReactDOM.render(
