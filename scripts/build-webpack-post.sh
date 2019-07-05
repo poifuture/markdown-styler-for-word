fd50e8c5855014cefc0f6203d56f269b39f9b0bd
Nagi <nagi@poifuture.com>
2019-07-05 15:35:32 +0800

    Release manifest

--- scripts/build-webpack-post.sh
+++ scripts/build-webpack-post.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # The only reason this script exists is for cross platform on Windows
-if [ "$1" != "node" ]; then
+if [ "$1" != "build" ]; then
   # pwd must be project root
-  echo "Please run 'npm run build:readme'"
+  echo "Please run 'yarn build'"
 fi
+
+cat ./manifest.xml | sed "s/https[:][/][/]localhost[:]3000/https:\/\/poifuture.github.io\/markdown-styler-for-word/g" > ./dist/manifest.xml
 
 cat ./src/README.md | sed "s/^.*prettier-ignore.*$/<!-- This file is auto generated, change src\/README.md instead. -->/g" | prettier --parser markdown > ./README.md
