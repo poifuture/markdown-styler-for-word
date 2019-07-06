fb1e3580b12751074de345358b57ef02d636e95b
Nagi <nagi@poifuture.com>
2019-07-06 13:16:56 +0800

    Release script to increase version

--- scripts/build-webpack-post.sh
+++ scripts/build-webpack-post.sh
@@ -5,6 +5,15 @@ if [ "$1" != "build" ]; then
   echo "Please run 'yarn build'"
 fi
 
-cat ./manifest.xml | sed "s/https[:][/][/]localhost[:]3000/https:\/\/poifuture.github.io\/markdown-styler-for-word/g" > ./dist/manifest.xml
+version=$(cat nextversion)
 
-cat ./src/README.md | sed "s/^.*prettier-ignore.*$/<!-- This file is auto generated, change src\/README.md instead. -->/g" | prettier --parser markdown > ./README.md
+cat ./manifest.xml \
+  | sed "s/https[:][/][/]localhost[:]3000/https:\/\/poifuture.github.io\/markdown-styler-for-word/g" \
+  | sed "s/[<]Version[>].*[<][/]Version[>]/<Version>0.$(date '+%Y%m%d').$version<\/Version>/g" \
+  > ./dist/manifest.xml
+
+cat ./src/README.md \
+  | sed "s/^.*prettier-ignore.*$/<!-- This file is auto generated, change src\/README.md instead. -->/g" \
+  | prettier --parser markdown > ./README.md
+
+echo $((version+1)) > nextversion
